a0ce6a1c933b5e62a26c1ccac937207cb6ad1019
Youlin Zhu <youlinzhu51@gmail.com>
2021-03-18 10:57:04 +0800

    批量化数据处理及 绘图代码完成

--- dataprocesssing.py
+++ dataprocesssing.py
@@ -1,28 +1,31 @@
 #该代码实现对于目标文件夹下pressure.profile的遍历、数据处理及绘图，作者Youlin 
+#运行前记得关闭excel、txt等程序的占用
 
 import pandas as pd
 import os
+
+input_filename = "pressure.profile" #原始数据文件名
+mergespace_file = "mergespace.txt"  #合并空格后文件名
+parse_file = "parse.xlsx"           #分列后文件名
+fullfill_file="fullfill.xlsx"       #填充后文件名
 
 
 # 变量初始化过程包括路径、变量名
 def main():
     rootdir=r"D:\Youlin Zhu\2021实验室\分子动力学\数据处理\strainrate"
+    global list
     list = os.listdir(rootdir)
- 
+
     for i in range(len(list)):
-        global path=os.path.join(rootdir,list[i])
-        global input_filename = "pressure.profile" #原始数据文件名
-        global mergespace_file = "mergespace.txt"  #合并空格后文件名
-        global parse_file = "parse.xlsx"               #分列后文件名
-        global fullfill_file="fullfill.xlsx"              #填充后文件名
+        global path
+        path = os.path.join(rootdir,list[i])
+        print ("我正在"+path+"下处理！")
         data_processing()
-       
-
-
+        
 # 将初始文件首行连续空格转化为一个写入一个新文件mergespace_filename
 def mergespace():
-    f=open (path+input_filename,"r")
-    f1=open(path+mergespace_file,"w")
+    f=open (path+'/'+input_filename,"r")
+    f1=open(path+'/'+mergespace_file,"w")
     data=f.readlines()
     for line in data:
         if line[0]==" ":
@@ -33,19 +36,18 @@ def mergespace():
     f.close()
     f1.close()
 
-
 #数据按空格分列后转化为数值写入excel,column为表头（第一行）名称
 def parse():
-    df = pd.read_csv(path+mergespace_file)
+    df = pd.read_csv(path+'/'+mergespace_file)
     column='# Chunk-averaged data for fix pressure_profile and group v_pressurexx'
     df= df[column].str.split(" ",expand=True)
     df.iloc[2:,:]=df.iloc[2:,:].apply(pd.to_numeric)
     df1=df.drop(labels=range(0,1),axis=0)
-    df1.to_excel(path+parse_file,index=False,header=0)
+    df1.to_excel(path+'/'+parse_file,index=False,header=0)  
 
 #填充时间，根据步长转化为ps
 def fullfill():
-    df=pd.read_excel(path+parse_file,sheetname=0,header=0)
+    df=pd.read_excel(path+'/'+parse_file,header=0)
     k=df.shape[0]
     for i in range(k):
         if pd.isna(df.iloc[i,0]):
@@ -53,12 +55,15 @@ def fullfill():
                 df.iloc[i,0]=df.iloc[i-1,0]/1000
             else:
                 df.iloc[i,0]=df.iloc[i-1,0]
-    df.to_excel(path+fullfill_file,index=False,merge_cell=False)
-    
-    
-#根据需要调用数据处理或者绘图函数        
-def data_processing
+    df.to_excel(path+'/'+fullfill_file,index=False)  
+
+#调用数据处理       
+def data_processing():
     mergespace()
     parse()
     fullfill()
 
+
+#运行主函数，等待收获数据
+main()    
+print("帮你处理了{}个文件，有点辛苦".format(len(list)))
--- draw.py
+++ draw.py
@@ -1,8 +1,12 @@
-#绘图代码:某时刻下参数（应力、温度、冲击方向速度）随切片位置分布的曲线绘制
+#该绘图代码根据需要筛选数据进行绘图，格式调整以及保存，作者Youlin Zhu
+
+#导入绘图库，表格文件路径及名称
 import matplotlib.pyplot as plt
+
 path=r"D:\Youlin Zhu\2021实验室\分子动力学\数据处理\strainrate\sample60\"
 fullfill_file="fullfill.xlsx"
 
+#定位最负压力（层裂）时刻的坐标-速度数据绘图，参数按需修改
 def draw_fixtime_curve():
     
     df=pd.read_excel(path+fullfill_file,sheet_name=0, header=0,index_col=0)
@@ -11,17 +15,18 @@ def draw_fixtime_curve():
     Tsp=df.idxmin()["v_pressurexx"] #定位时刻，可手动改为 XX (ps)
     xaxis=df.loc[Tsp,goal_xaxis].values
     yaxis=df.loc[Tsp,goal_yaxis].values
+    
     plt.figure(num=1)
     plt.plot(xaxis,yaxis,color='blue',linewidth=1.5)
     plt.rcParams['xtick.direction'] = 'in'
     plt.rcParams['ytick.direction'] = 'in'
-    #plt.xticks([200,400,600,800,1000]，fontproperties = 'Times New Roman', size = 14)
-    #plt.yticks([0,2,4,6,8],[0,0.2,0.4,0.6,0.8]，fontproperties = 'Times New Roman', size = 14)
+    plt.xticks([200,400,600,800,1000]，fontproperties = 'Times New Roman', size = 14)
+    plt.yticks([0,2,4,6,8],[0,0.2,0.4,0.6,0.8]，fontproperties = 'Times New Roman', size = 14)
     plt.xlabel("Position (nm)",fontdict={'family' : 'Times New Roman', 'size'   : 16})
     plt.ylabel("Velocity Z (km/s)"，fontdict={'family' : 'Times New Roman', 'size'   : 16})
-    #plt.savefig(r"C:\Users\Youlin Zhu\Desktop\1.tiff"，dpi=600) 
+    #plt.savefig(r"C:\Users\Youlin Zhu\Desktop\1.tiff"，dpi=600) #保存
     
-    
+#提取自由表面速度数据绘图，参数按需修改  
 def draw_freesurfaceVelocity():
     axistime=[]
     axisvelocity=[]
@@ -40,5 +45,7 @@ def draw_freesurfaceVelocity():
     plt.yticks([0,2,4,6,8,10],[0,0.2,0.4,0.6,0.8,1.0],fontproperties = 'Times New Roman', size = 14)
     plt.xlabel("Time (ps)",fontdict={'family' : 'Times New Roman', 'size'   : 16})
     plt.ylabel("Velocity Z (km/s)",fontdict={'family' : 'Times New Roman', 'size'   : 16})
-    plt.savefig(r"C:\Users\Youlin Zhu\Desktop\1.tiff",dpi=600)
-    
+    #plt.savefig(r"C:\Users\Youlin Zhu\Desktop\1.tiff",dpi=600)
+ 
+draw_fixtime_curve()
+draw_freesurfaceVelocity() 
